670e09912901de410c50e3a4374e3f04ed39fe17
Jeremy Poinas <poinas.jeremy@gmail.com>
2023-07-20 10:17:04 +0200

    Flexible mines

--- Minesweeper (Java)/task/src/minesweeper/Main.java
+++ Minesweeper (Java)/task/src/minesweeper/Main.java
@@ -1,16 +1,37 @@
 package minesweeper;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("""
-                .X.......
-                .....X..X
-                ....X....
-                ......X..
-                ..X......
-                ....X....
-                ..X......
-                ..X......
-                ......X..""");
+        System.out.println("How many mines do you want on the field?");
+        Scanner scanner = new Scanner(System.in);
+        int mines = scanner.nextInt();
+        createField(mines);
+    }
+
+    private static void createField(int mines) {
+        StringBuilder line;
+        StringBuilder field = new StringBuilder();
+        int restingMines = mines;
+
+        while (restingMines > 0) {
+            restingMines = mines;
+            field = new StringBuilder();
+            for (int i = 0; i < 9; i++) {
+                line = new StringBuilder();
+                for (int j = 0; j < 9; j++) {
+                    if (restingMines > 0 && Math.random() > 0.6) {
+                        line.append('X');
+                        restingMines--;
+                    } else {
+                        line.append('.');
+                    }
+                }
+                line.append("\n");
+                field.append(line);
+            }
+        }
+        System.out.println(field);
     }
 }
