e6da7afe32bd6a24022ff1fe527447bcdcc1c4f2
Jeremy Poinas <poinas.jeremy@gmail.com>
2023-07-26 18:26:17 +0200

    Finished project

--- Minesweeper (Java)/task/src/minesweeper/Main.java
+++ Minesweeper (Java)/task/src/minesweeper/Main.java
@@ -1,26 +1,41 @@
 package minesweeper;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("How many mines do you want on the field?");
+        System.out.print("How many mines do you want on the field?  ");
         Scanner scanner = new Scanner(System.in);
         int minesTarget = scanner.nextInt();
         scanner.nextLine();
         Minesweeper minesweeper = new Minesweeper(minesTarget);
 
         while (!minesweeper.hasWonTheGame()) {
-            System.out.println("Set/delete mine marks (x and y coordinates):");
+            System.out.print("Set/unset mine marks or claim a cell as free:  ");
+
             String input = scanner.nextLine();
             int y = Integer.parseInt(input.split(" ")[0]) - 1;
             int x = Integer.parseInt(input.split(" ")[1]) - 1;
-            if (Arrays.asList('.', '*').contains(minesweeper.field[x][y])) {
-                minesweeper.markCell(x, y);
-                minesweeper.printField();
-            } else {
-                System.out.println("There is a number here!");
+            String move = input.split(" ")[2];
+
+            if (Objects.equals(move, "mine")) {
+                if (Arrays.asList('.', '*').contains(minesweeper.field[x][y])) {
+                    minesweeper.markCell(x, y);
+                    minesweeper.printField();
+                } else {
+                    System.out.println("There is a number here!");
+                }
+            } else if (Objects.equals(move, "free")) {
+                if (minesweeper.freeCell(x, y)) {
+                    minesweeper.printField();
+                } else {
+                    minesweeper.revealMines();
+                    minesweeper.printField();
+                    System.out.println("You stepped on a mine and failed!");
+                    return;
+                }
             }
         }
         System.out.println("Congratulations! You found all mines!");
--- Minesweeper (Java)/task/src/minesweeper/Minesweeper.java
+++ Minesweeper (Java)/task/src/minesweeper/Minesweeper.java
@@ -1,92 +1,81 @@
 package minesweeper;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 
 public class Minesweeper {
+    private final int minesTarget;
     ArrayList<String> mines = new ArrayList<>();
 
     ArrayList<String> marked = new ArrayList<>();
 
     char[][] field = new char[9][9];
 
+    int revealed = 0;
+
     public Minesweeper(int minesTarget) {
-        createMines(minesTarget);
+        this.minesTarget = minesTarget;
         createField();
         printField();
     }
 
-    private void createMines(int minesTarget) {
-        Random random = new Random();
-        ArrayList<String> mines = new ArrayList<>();
-        int minesOnField = 0;
+    private void createMines(int i, int j) {
+        ArrayList<String> possibilities =  new ArrayList<>(81);
+        for (int a = 0; a < 9; a++) {
+            for (int b = 0; b < 9; b++) {
+                if (a != i || b != j) {
+                    possibilities.add(a + " " + b);
+                }
+            }
+        }
 
+        Random random = new Random();
+        int minesOnField = 0;
         while (minesOnField < minesTarget) {
-            int x = random.nextInt(9);
-            int y = random.nextInt(9);
+            int index = random.nextInt(possibilities.size());
+            int x = Integer.parseInt(possibilities.get(index).split(" ")[0]);
+            int y = Integer.parseInt(possibilities.get(index).split(" ")[1]);
             if (!mines.contains(x + " " + y)) {
                 mines.add(x + " " + y);
+                possibilities.remove(x + " " + y);
                 minesOnField++;
             }
         }
-        this.mines = mines;
     }
 
     void createField() {
         for (char[] chars: field) {
             Arrays.fill(chars, '.');
         }
-
-        for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field[0].length; j++) {
-                if (!mines.contains(i + " " + j)) {
-                    calculateMinesAroundEmptyCell(i, j);
-                }
-            }
-        }
     }
 
     void calculateMinesAroundEmptyCell(int i, int j) {
-        ArrayList<Integer> iPositions = new ArrayList<>();
-        ArrayList<Integer> jPositions = new ArrayList<>();
+        ArrayList<String> toExplore = new ArrayList<>();
+        toExplore.add(i + " " + j);
 
-        if (i == 0) {
-            iPositions.add(0);
-            iPositions.add(1);
-        } else if (i == field.length - 1) {
-            iPositions.add(0);
-            iPositions.add(-1);
-        } else {
-            iPositions.add(0);
-            iPositions.add(-1);
-            iPositions.add(1);
-        }
-
-        if (j == 0) {
-            jPositions.add(0);
-            jPositions.add(1);
-        } else if (j == field[0].length - 1) {
-            jPositions.add(0);
-            jPositions.add(-1);
-        } else {
-            jPositions.add(0);
-            jPositions.add(-1);
-            jPositions.add(1);
-        }
-
-        int minesCount = 0;
-        for (Integer x: iPositions) {
-            for (Integer y: jPositions) {
-                int deltaX = i + x;
-                int deltaY = j + y;
-                if (mines.contains(deltaX + " " + deltaY)) {
-                    minesCount++;
+        while (!toExplore.isEmpty()) {
+            ArrayList<String> toAdd = new ArrayList<>();
+            int minesCount = 0;
+            String curr = toExplore.remove(0);
+            int currX = Integer.parseInt(curr.split(" ")[0]);
+            int currY = Integer.parseInt(curr.split(" ")[1]);
+            for (int x = currX - 1; x <= currX + 1; x++) {
+                for (int y = currY - 1; y <= currY + 1; y++) {
+                    if (x >= 0 && y >= 0 && x < 9 && y < 9 && (x != currX || y != currY)) {
+                        if (mines.contains(x + " " + y)) {
+                            minesCount++;
+                        } else if (Arrays.asList('.', '*').contains(field[x][y]) && !toExplore.contains(x + " " + y)) {
+                            toAdd.add(x + " " + y);
+                        }
+                    }
                 }
             }
-        }
-        if (minesCount > 0) {
-            field[i][j] = Character.forDigit(minesCount,10);
+            if (minesCount > 0) {
+                field[currX][currY] = Character.forDigit(minesCount,10);
+            } else {
+                field[currX][currY] = '/';
+                toExplore.addAll(toAdd);
+            }
+            revealed++;
         }
     }
 
@@ -107,6 +96,14 @@ public class Minesweeper {
     }
 
     boolean hasWonTheGame() {
+        if (mines.size() == 0) {
+            return false;
+        }
+
+        if (revealed == 9 * 9 - mines.size()) {
+            return true;
+        }
+
         if (marked.size() != mines.size()) {
             return false;
         }
@@ -126,6 +123,25 @@ public class Minesweeper {
         } else {
             marked.add(x + " " + y);
             field[x][y] = '*';
+        }
+    }
+
+    public boolean freeCell(int x, int y) {
+        if (mines.size() == 0)  {
+            createMines(x, y);
+        }
+        if (mines.contains(x + " " + y)) {
+            return false;
+        }
+        calculateMinesAroundEmptyCell(x, y);
+        return true;
+    }
+
+    public void revealMines() {
+        for (String mine: mines) {
+            int currX = Integer.parseInt(mine.split(" ")[0]);
+            int currY = Integer.parseInt(mine.split(" ")[1]);
+            field[currX][currY] = 'X';
         }
     }
 }
